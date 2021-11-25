1880d4c5b819b5299b28a7a3593bce46c21ba139
karenlander <karen8lander@hotmail.com>
2021-11-25 11:42:18 +0200

    Person implementation in progress

--- src/OOP2/Solution/PersonImpl.java
+++ src/OOP2/Solution/PersonImpl.java
@@ -5,49 +5,93 @@ import OOP2.Provided.Person;
 import OOP2.Provided.SamePersonException;
 import OOP2.Provided.Status;
 
-import java.util.Collection;
+import java.util.*;
 
-public class PersonImpl implements Person {
-
+public class PersonImpl implements Person{
+	private Integer id;
+	private String name;
+	private List<Status> statuses;
+	private Integer nextIdStatus;
+	private Set<Person> friends;
 	/**
 	 * Constructor receiving person's id and name.
 	 */
 	public PersonImpl(Integer id, String name)
-	{}
+	{
+		this.id = id;
+		this.name = name;
+		this.statuses = new ArrayList<>();
+		this.nextIdStatus = 0;
+		this.friends = new HashSet<>();
+	}
 
 	@Override
 	public Integer getId() {
-		return null;
+		return this.id;
 	}
 
 	@Override
 	public String getName() {
-		return null;
+		return this.name;
 	}
 
 	@Override
 	public Status postStatus(String content) {
-		return null;
+		Status newStatus = new StatusImpl(this, content, nextIdStatus);
+		this.nextIdStatus += 1;
+		this.statuses.add(newStatus);
+		return newStatus;
 	}
 
 	@Override
 	public void addFriend(Person p) throws SamePersonException, ConnectionAlreadyExistException {
-
+		if (this.equals(p)) {
+			throw new SamePersonException();
+		}
+		if (this.friends.contains(p)){
+			throw new ConnectionAlreadyExistException();
+		}
+		this.friends.add(p);
+		p.addFriend(this); //TODO: assuming p is reference (check)
 	}
 
 	@Override
 	public Collection<Person> getFriends() {
-		return null;
+		return this.friends;
 	}
 
 	@Override
 	public Iterable<Status> getStatusesRecent() {
-		return null;
+		List<Status> copyStatuses = new ArrayList<>(this.statuses) ;
+		copyStatuses.sort(new Comparator<Status>() {
+			@Override
+			public int compare(Status o1, Status o2) {
+				return o2.getId() - o1.getId();
+			}
+		});
+		return copyStatuses;
 	}
 
 	@Override
 	public Iterable<Status> getStatusesPopular() {
-		return null;
+		List<Status> copyStatuses = new ArrayList<>(this.statuses) ;
+		copyStatuses.sort(new Comparator<Status>() {
+			@Override
+			public int compare(Status o1, Status o2) {
+				int diff = o2.getLikesCount() - o1.getLikesCount();
+				if (diff == 0 ){
+					return o2.getId() - o1.getId();
+				}
+				return diff;
+			}
+		});
+		return copyStatuses;
+	}
+
+
+	@Override
+	public boolean equals(Object o){
+		return false;
 	}
 
 	@Override
--- src/hello.java
+++ src/hello.java
@@ -1,6 +1,8 @@
 // Your First Program
 
 import OOP2.Provided.Person;
+import OOP2.Provided.Status;
+import OOP2.Solution.PersonImpl;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -8,13 +10,31 @@ import java.util.Set;
 class hello {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        Set<Integer> likes  =  new HashSet<>();
-        likes.add(5);
-        System.out.println(likes.size());
-        likes.add(5);
-        System.out.println(likes.size());
-        likes.remove(4);
-        System.out.println(likes.size());
+        Person p1 = new PersonImpl(1,"dor1");
+        Person p2 = new PersonImpl(2,"dor2");
+        Person p3 = new PersonImpl(3,"dor3");
+        Person p4 = new PersonImpl(4,"dor4");
+        Person p5 = new PersonImpl(5,"dor5");
+
+
+        Status s1 = p1.postStatus("status 1");
+        Status s2 = p1.postStatus("status 2");
+        Status s3 = p1.postStatus("status 3");
+
+        s1.like(p2);
+        s1.like(p3);
+        s2.like(p2);
+        s2.like(p3);
+        s2.like(p4);
+        s3.like(p2);
+        s3.like(p2);
+        s3.like(p2);
+        s3.like(p2);
+
+        Iterable<Status> list = p1.getStatusesPopular();
+        for (Status it : list){
+            System.out.println(it.getContent());
+        }
     }
 
     public static int id(int x){
